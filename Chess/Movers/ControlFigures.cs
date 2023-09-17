72649de2f79c229a8ecde3c915e54493e4716435
Oliver Hruban <HrubanOliver@sssvt.cz>
2023-09-17 19:26:16 +0200

    bishop and queen validate method

--- Chess/Movers/ControlFigures.cs
+++ Chess/Movers/ControlFigures.cs
@@ -14,27 +14,33 @@ namespace Chess.Movers
     public class ControlFigures
     {
         private Grid? _board { get; set; }
-        private readonly List<char> _xDirections = new List<char>() { 'A','B', 'C', 'D', 'E', 'F', 'G', 'H'};
+        private readonly List<char> _xDirections = new List<char>() { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' };
 
 
-        public ControlFigures(Grid board) 
-        { 
+        public ControlFigures(Grid board)
+        {
             this._board = board;
         }
 
-        
+
         public bool PossibleMove(string Figure, string from, string to)
         {
             switch (Figure)
             {
                 case "\u2659":
-                    return PawnValidation(from, to, true);
+                    return ValidatePawn(from, to, true);
 
                 case "\u2658":
-                    return KnightValidation(from, to);
+                    return ValidateKnight(from, to);
 
                 case "\u2656":
-                    return RockValidation(from, to);
+                    return ValidateRock(from, to);
+
+                case "\u2657":
+                    return ValidateBishop(from, to);
+
+                case "\u2655":
+                    return ValidateQueen(from, to);
 
 
                 default:
@@ -46,27 +52,27 @@ namespace Chess.Movers
         public Label ReturnPosition(string directions)
         {
             Object obj = _board.FindName(directions);
-            return(Label)obj;
+            return (Label)obj;
         }
 
-        public bool PawnValidation(string from, string to, bool isWhite)
+        public bool ValidatePawn(string from, string to, bool isWhite)
         {
             if (isWhite)
             {
-                if(ReturnPosition(to).Content != "")
+                if (ReturnPosition(to).Content != "")
                 {
-                    if (Math.Pow(Convert.ToInt16(to[1]) - Convert.ToInt16(from[1]),2) == 1 && Math.Pow(_xDirections.IndexOf(to[0]) - _xDirections.IndexOf(from[0]),2) == 1)
+                    if (Math.Pow(Convert.ToInt16(to[1]) - Convert.ToInt16(from[1]), 2) == 1 && Math.Pow(_xDirections.IndexOf(to[0]) - _xDirections.IndexOf(from[0]), 2) == 1)
                     {
                         return true;
                     }
                 }
 
-                if (from[0] == to[0] && (Convert.ToInt16(from[1]) - Convert.ToInt16(to[1]) == -1 )) 
+                if (from[0] == to[0] && (Convert.ToInt16(from[1]) - Convert.ToInt16(to[1]) == -1))
                 {
                     if (ReturnPosition(to).Content == "")
                     {
                         return true;
-                    }                                      
+                    }
                 }
                 else if (from[1] == '2' && to[1] == '4')
                 {
@@ -81,7 +87,7 @@ namespace Chess.Movers
                 }
                 return false;
             }
-            return false;           
+            return false;
         }
 
         private bool _mark;
@@ -91,15 +97,24 @@ namespace Chess.Movers
             if (_mark)
             {
                 _mark = false;
-                return N1 + N2;              
+                return N1 + N2;
             }
             _mark = true;
             return N1 - N2;
         }
 
+        public bool ValidateQueen(string from, string to)
+        {
+            if (ValidateRock(from, to) || ValidateBishop(from, to))
+            {
+                return true;
+            }
+            return false;
+        }
+
         //kvůli královi musím storovat všechny políčka který figurky napadaj
 
-        public bool KnightValidation(string from, string to)
+        public bool ValidateKnight(string from, string to)
         {
             _mark = true;
             string possibleDir = "";
@@ -111,9 +126,9 @@ namespace Chess.Movers
             {
                 //vertikal validation
                 row = ChangeMark(Convert.ToInt32(from[1].ToString()), 2);
-                
 
-                if (_xDirections.IndexOf(from[0]) +1 < _xDirections.Count)
+
+                if (_xDirections.IndexOf(from[0]) + 1 < _xDirections.Count)
                 {
                     column = _xDirections[_xDirections.IndexOf(from[0]) + 1];
                 }
@@ -172,19 +187,19 @@ namespace Chess.Movers
             return false;
         }
 
-        //queen = rock and bishop
-        public bool RockValidation(string from, string to)
+
+        public bool ValidateRock(string from, string to)
         {
             string columns = from[0].ToString();
 
-            for (int i = Convert.ToInt32(from[1].ToString()) +1; i < 9; i++)
+            for (int i = Convert.ToInt32(from[1].ToString()) + 1; i < 9; i++)
             {
                 columns = columns[0] + i.ToString();
-                if(columns == to)
+                if (columns == to)
                 {
                     return true;
                 }
-                else if(ReturnPosition(columns).Content != "")
+                else if (ReturnPosition(columns).Content != "")
                 {
                     break;
                 }
@@ -192,7 +207,7 @@ namespace Chess.Movers
 
             columns = from[0].ToString();
 
-            for (int i = Convert.ToInt32(from[1].ToString())-1; i > 0; i--)
+            for (int i = Convert.ToInt32(from[1].ToString()) - 1; i > 0; i--)
             {
                 columns = columns[0] + i.ToString();
                 if (columns == to)
@@ -237,6 +252,170 @@ namespace Chess.Movers
 
             return false;
         }
+
+
+        public bool ValidateBishop(string from, string to)
+        {
+            string currentCell = "";
+            int a = _xDirections.IndexOf(from[0]);
+
+            //left up
+            for (int i = Convert.ToInt32(from[1].ToString()) + 1; i < 9; i++)
+            {
+                if(from == to)
+                {
+                    continue;
+                }
+
+
+                if(a == 0)
+                {                  
+                    currentCell = _xDirections[a] + i.ToString();
+                    a--;
+                }
+                else if (a > 0)
+                {
+                    a--;
+                    currentCell = _xDirections[a] + i.ToString();
+                    
+                }
+                
+                if (currentCell == to)
+                {
+                    return true;
+                }
+                else if (ReturnPosition(currentCell).Content != "")
+                {
+                    break;
+                }                          
+            }
+
+            currentCell = "";
+            a = _xDirections.IndexOf(from[0]);
+
+            //left down
+            for (int i = Convert.ToInt32(from[1].ToString()) - 1; i > 0; i--)
+            {
+                if (from == to)
+                {
+                    continue;
+                }
+
+
+                if (a == 0)
+                {
+                    currentCell = _xDirections[a] + i.ToString();
+                    a--;
+                }
+                else if (a > 0)
+                {
+                    a--;
+                    currentCell = _xDirections[a] + i.ToString();
+                    
+                }
+                
+                if (currentCell == to)
+                {
+                    return true;
+                }
+                else if (ReturnPosition(currentCell).Content != "")
+                {
+                    break;
+                }
+                
+                    
+            }
+
+            currentCell = "";
+            a = _xDirections.IndexOf(from[0]);
+
+            //right up
+            for (int i = Convert.ToInt32(from[1].ToString()) + 1; i < 9; i++)
+            {
+                if (from == to)
+                {
+                    continue;
+                }
+
+                if (a == 8)
+                {
+                    currentCell = _xDirections[a] + i.ToString();
+                    a++;
+                }
+                else if (a < 7)
+                {
+                    a++;
+                    currentCell = _xDirections[a] + i.ToString();
+                    
+                }
+
+                try
+                {
+                    if (currentCell == to)
+                    {
+                        return true;
+                    }
+                    else if (ReturnPosition(currentCell).Content != "")
+                    {
+                        break;
+                    }
+                }
+                catch
+                {
+                }
+                
+                
+              
+
+            }
+
+            currentCell = "";
+            a = _xDirections.IndexOf(from[0]);
+
+            //right down
+            for (int i = Convert.ToInt32(from[1].ToString()) - 1; i > 0; i--)
+            {
+
+                if (from == to)
+                {
+                    continue;
+                }
+
+                if (a == 8)
+                {
+                    a++;
+                    currentCell = _xDirections[a] + i.ToString();
+
+                }
+                else if (a < 7)
+                {
+                    a++;
+                    currentCell = _xDirections[a] + i.ToString();
+                    
+                }
+
+                try
+                {
+                    if (currentCell == to)
+                    {
+                        return true;
+                    }
+                    else if (ReturnPosition(currentCell).Content != "")
+                    {
+                        break;
+                    }
+                }
+                catch { 
+                }
+              
+               
+
+            }
+
+            return false;
+        }
+
+
 
     }
 }
